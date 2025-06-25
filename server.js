967f5d7f8a21c6b9a0d70dea3f51f07aa7196ba1
mthq2004 <maithanhhaiquan2004@gmail.com>
2025-06-25 11:53:15 +0700

    Add root GET route

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ app.use("/api/contacts", contactRoutes);
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => app.listen(process.env.PORT, () => console.log("Server running on port", process.env.PORT)))
   .catch(err => console.error("MongoDB error:", err));
+
+  app.get('/', (req, res) => {
+  res.send('API is running...');
+});
